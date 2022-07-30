34e010e3ded5fca5d6e64202479cd55f775b7797
NursaidNart <nursaidnart@gmail.com>
2022-07-30 04:04:21 +0300

    # add Vue3 vue-router and axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 import './bootstrap';
+
+import App from './components/App.vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import { routes } from './routes';
+import * as VueRouter from 'vue-router';
+import * as Vue from 'vue'
+
+
+const router =  VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes: routes
+});
+
+
+const app = Vue.createApp(App);
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app');
+
+
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+
+let mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js').vue({
+    extractStyles: true,
+    globalStyles: false
+});
