021415fdf578ec9cd6e80792c14620c8f885698b
NursaidNart <nursaidnart@gmail.com>
2022-08-19 14:13:15 +0300

    # login controller added

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,100 @@
+@extends('layout')
+
+@section('content')
+<main class="login-form">
+  <div class="cotainer">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header">Login</div>
+                  <div class="card-body">
+
+                      <form action="{{ route('login.post') }}" method="POST">
+                          @csrf
+
+                          {{-- <div class="form-group row">
+
+                                <label for="exampleInputEmail1" class="col-md-4 col-form-label text-md-right" >Email address</label>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+
+                          </div>
+                          <div class="form-group row">
+
+                              <label for="exampleInputPassword1" class="col-md-4 col-form-label text-md-right">Password</label>
+                              <div class="col-md-6">
+                              <input type="password" class="form-control" id="exampleInputPassword1">
+                              </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <div class="mb-3 form-check col-md-6 offset-md-4">
+                              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                              <div class="col-md-6">
+                              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                Login
+                            </button>
+                        </div>
+
+                            ------------------------------------------------------------------ --}}
+
+                          <div class="form-group row">
+                              <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                              <div class="col-md-6">
+                                  <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+                                  @if ($errors->has('email'))
+                                      <span class="text-danger">{{ $errors->first('email') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+
+                          <div class="form-group row">
+                              <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                              <div class="col-md-6">
+                                  <input type="password" id="password" class="form-control" name="password" required>
+                                  @if ($errors->has('password'))
+                                      <span class="text-danger">{{ $errors->first('password') }}</span>
+                                  @endif
+                              </div>
+                          </div>
+
+                          <div class="form-group row">
+                              <div class="col-md-6 offset-md-4">
+                                  <div class="checkbox">
+                                      <label>
+                                          <input type="checkbox" name="remember"> Remember Me
+                                      </label>
+                                  </div>
+                              </div>
+                          </div>
+
+                          <div class="col-md-6 offset-md-4">
+                              <button type="submit" class="btn btn-primary">
+                                  Login
+                              </button>
+                          </div>
+                      </form>
+
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</main>
+@endsection
